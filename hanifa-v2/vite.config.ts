73f0d389f9306c04c295279f8b49b498ea987720
Bulantsev Oleg <macbook@MacBook-Pro.local>
2025-09-24 00:59:51 +0300

    small addons with envs

--- hanifa-v2/vite.config.ts
+++ hanifa-v2/vite.config.ts
@@ -5,10 +5,18 @@ import path from "path";
 export default defineConfig({
   base: "./",
   plugins: [react()],
+  define: {
+    "import.meta.env.VITE_BUCKET_BASE": JSON.stringify(process.env.VITE_BUCKET_BASE),
+    "import.meta.env.VITE_SIGN_URL": JSON.stringify(process.env.VITE_SIGN_URL),
+    "import.meta.env.VITE_COMMIT_URL": JSON.stringify(process.env.VITE_COMMIT_URL),
+    "import.meta.env.VITE_ADMIN_PASSWORD": JSON.stringify(process.env.VITE_ADMIN_PASSWORD),
+  },  
   resolve: {
     alias: { "@": path.resolve(__dirname, "src") }
   }
 });
+
+
 
 // vite.config.ts
 // import { defineConfig } from "vite";
