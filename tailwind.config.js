6acfc5aa16c6307a6aafbff3f4cf6fe1c3adedde
Bulantsev Oleg <macbook@MacBook-Pro.local>
2025-09-24 09:39:28 +0300

    add new fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        // базовый шрифт на сайте
+        sans: ['Montserrat-Regular', 'RussischSans', 'system-ui', 'Segoe UI', 'Roboto', 'Arial', 'sans-serif'],
+        // отдельный стек для заголовков (по желанию)
+        display: ['Montserrat-Regular', 'RussischSans', 'system-ui', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
+};
 
